84ff31a6c3e1af114f369b3d67fc8ab1a31b707b
Lý Đông Cảnh <lydongcanh@gmail.com>
2020-06-16 01:41:12 +0700

    [ControllerApp] install react-native-bluetooth-classic.

--- ArduinoSketch/ArduinoSketch.ino
+++ ArduinoSketch/ArduinoSketch.ino
@@ -4,7 +4,6 @@
 #define RX_PIN      6
  
 SoftwareSerial bluetooth(RX_PIN, TX_PIN);
-int baudRate[] = {300, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200};
  
 void setup() {
   Serial.begin(9600);
@@ -12,25 +11,16 @@ void setup() {
   
   Serial.println("Configuring, please wait...");
   
-  for (int i = 0 ; i < 9 ; i++) {
-     bluetooth.begin(baudRate[i]);
-     String cmd = "AT+BAUD4";
-     bluetooth.print(cmd);
-     bluetooth.flush();
-     delay(100);
-  }
-  
   bluetooth.begin(9600);
   Serial.println("Config done");
   while (!bluetooth) {}
-  
-  Serial.println("Enter AT commands:");
-}
+  }
  
 void loop() {
   if (bluetooth.available()) {
     Serial.write(bluetooth.read());
   }
+  
   if (Serial.available()) {
     bluetooth.write(Serial.read());
   }
--- ControllerApp/App.js
+++ ControllerApp/App.js
@@ -1,9 +1,113 @@
-import React from "react";
-import { View, Text } from "react-native";
-import { ActivityIndicator } from "react-native-paper";
+import React, { useState, useEffect } from "react";
+import { View, FlatList, Platform, PermissionsAndroid } from "react-native";
+import { Text, Button, Card, ActivityIndicator, List, TextInput } from "react-native-paper";
+import RNBluetoothClassic, { BTEvents, BTCharsets } from "react-native-bluetooth-classic";
 
 export default function App() {
+    const [devices, setDevices] = useState([]);
+    const [sendValue, setSendValue] = useState("");
+    const [isLoading, setisLoading] = useState(false);
+    const [isConnected, setIsConnected] = useState(false);
+    const [connectId, setConnectId] = useState();
+    
+    useEffect(() => {
+        checkAndrdoiPermission();
+    }, []);
+
+    async function startScan() {
+        setDevices([]);
+        setisLoading(true);
+
+        const list = await RNBluetoothClassic.list();
+        setDevices(list);
+        setisLoading(false);
+    }
+
+    async function connect(id) {
+        try {
+            setisLoading(true);
+            const result = await RNBluetoothClassic.connect(id);
+            setIsConnected(true);
+            setConnectId(id);
+            console.log(result);
+        } catch(e) {
+            setIsConnected(false);
+            console.log(e);
+        } finally {
+            setisLoading(false);
+        }
+    }
+
+    async function disconnect() {
+        const result = await RNBluetoothClassic.disconnect();
+        setIsConnected(false);
+        setConnectId();
+        console.log(result);
+    }
+
+    async function send() {
+        RNBluetoothClassic.write(sendValue);
+    }
+
+    function checkAndrdoiPermission() {
+        if (Platform.OS === 'android' && Platform.Version >= 23) {
+            PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION).then((result) => {
+                if (result) {
+                    console.log("Permission is OK");
+                } else {
+                    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION).then((result) => {
+                        if (result) {
+                            console.log("User accept");
+                        } else {
+                            console.log("User refuse");
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    function renderDeviceList() {
+        return (
+            <List.Section title="Available devices">
+                {devices.map(device => {
+                    const borderWidth = device.id == connectId ? 1 : 0;
+                    return (
+                        <List.Item
+                            disabled={isConnected}
+                            onPress={async () => await connect(device.id)}
+                            onLongPress={() => alert(JSON.stringify(device, null, 2))}
+                            key={device.id}
+                            title={device.name}
+                            description={device.id}
+                            style={{ borderWidth: borderWidth, borderColor: "green", borderRadius: 4 }}
+                        />
+                    )
+                })}
+            </List.Section>
+        );
+    }
+
     return (
-        <ActivityIndicator />
+        <View style={{ flex: 1, justifyContent: "space-between", margin: 12 }}>
+            {renderDeviceList()}
+            <View>
+                <TextInput
+                    label="Send value"
+                    value={sendValue}
+                    mode="outlined"
+                    onChangeText={setSendValue}
+                    style={{ marginVertical: 4 }}
+                />
+                <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                    <View style={{ flexDirection: "row" }}>
+                        <Button onPress={startScan}>Scan</Button>
+                        <Button disabled={!isConnected} onPress={send}>Send</Button>
+                        <Button disabled={!isConnected} onPress={disconnect}>Disconnect</Button>
+                    </View>
+                    <ActivityIndicator animating={isLoading} hidesWhenStopped={true} />
+                </View>
+            </View>
+        </View>
     );
 }
