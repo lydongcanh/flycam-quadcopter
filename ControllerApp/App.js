c275a4398430635535d75d564ec528f620744c03
Lý Đông Cảnh <lydongcanh@gmail.com>
2020-06-16 16:16:09 +0700

    [ControllerApp] Add control buttons.

--- ControllerApp/App.js
+++ ControllerApp/App.js
@@ -1,15 +1,31 @@
 import React, { useState, useEffect } from "react";
-import { View, FlatList, Platform, PermissionsAndroid } from "react-native";
-import { Text, Button, Card, ActivityIndicator, List, TextInput } from "react-native-paper";
+import { View, Platform, PermissionsAndroid, StyleSheet } from "react-native";
+import { Button, Caption, Card, ActivityIndicator, List } from "react-native-paper";
 import RNBluetoothClassic, { BTEvents, BTCharsets } from "react-native-bluetooth-classic";
 
 export default function App() {
+
+    const styles = StyleSheet.create({
+        controlButtons: {
+            width: "24%",
+            height: "95%",
+            alignSelf: "center",
+            alignItems: "center",
+            paddingTop: 15
+        },
+        controlTexts: {
+            textAlignVertical: "center",
+            alignSelf: "center",
+            alignContent: "center",
+            alignItems: "center"
+        }
+    });
+
     const [devices, setDevices] = useState([]);
-    const [sendValue, setSendValue] = useState("");
     const [isLoading, setisLoading] = useState(false);
     const [isConnected, setIsConnected] = useState(false);
     const [connectId, setConnectId] = useState();
-    
+
     useEffect(() => {
         checkAndrdoiPermission();
     }, []);
@@ -30,9 +46,10 @@ export default function App() {
             setIsConnected(true);
             setConnectId(id);
             console.log(result);
-        } catch(e) {
+        } catch (e) {
             setIsConnected(false);
             console.log(e);
+            alert(e);
         } finally {
             setisLoading(false);
         }
@@ -45,8 +62,8 @@ export default function App() {
         console.log(result);
     }
 
-    async function send() {
-        RNBluetoothClassic.write(sendValue);
+    function send(sendValue) {
+        RNBluetoothClassic.write(sendValue.toString());
     }
 
     function checkAndrdoiPermission() {
@@ -88,21 +105,49 @@ export default function App() {
         );
     }
 
+    function renderControlButtons() {
+        return (
+            <Card style={{ padding: 4, backgroundColor: "rgba(0, 0, 0, 0.05)" }}>
+                <View style={{ height: 60, flexDirection: "row", justifyContent: "space-between" }}>
+                        <Card style={styles.controlButtons} onPress={() => send(0)}>
+                            <Caption style={styles.controlTexts}>Go down</Caption>
+                        </Card>
+                        <Card style={styles.controlButtons} onPress={() => send(1)}>
+                            <Caption style={styles.controlTexts}>Go up</Caption>
+                        </Card>
+                        <Card style={styles.controlButtons} onPress={() => send(2)}>
+                            <Caption style={styles.controlTexts}>Go forward</Caption>
+                        </Card>
+                        <Card style={styles.controlButtons} onPress={() => send(3)}>
+                            <Caption style={styles.controlTexts}>Go backward</Caption>
+                        </Card>
+                </View>
+                <View style={{ height: 60, flexDirection: "row", justifyContent: "space-between" }}>
+                        <Card style={styles.controlButtons} onPress={() => send(4)}>
+                            <Caption style={styles.controlTexts}>Bend left</Caption>
+                        </Card>
+                        <Card style={styles.controlButtons} onPress={() => send(5)}>
+                            <Caption style={styles.controlTexts}>Bend right</Caption>
+                        </Card>
+                        <Card style={styles.controlButtons} onPress={() => send(6)}>
+                            <Caption style={styles.controlTexts}>Rotate left</Caption>
+                        </Card>
+                        <Card style={styles.controlButtons} onPress={() => send(7)}>
+                            <Caption style={styles.controlTexts}>Rotate right</Caption>
+                        </Card>
+                </View>
+            </Card>
+        );
+    }
+
     return (
         <View style={{ flex: 1, justifyContent: "space-between", margin: 12 }}>
             {renderDeviceList()}
             <View>
-                <TextInput
-                    label="Send value"
-                    value={sendValue}
-                    mode="outlined"
-                    onChangeText={setSendValue}
-                    style={{ marginVertical: 4 }}
-                />
+                {renderControlButtons()}
                 <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
                     <View style={{ flexDirection: "row" }}>
                         <Button onPress={startScan}>Scan</Button>
-                        <Button disabled={!isConnected} onPress={send}>Send</Button>
                         <Button disabled={!isConnected} onPress={disconnect}>Disconnect</Button>
                     </View>
                     <ActivityIndicator animating={isLoading} hidesWhenStopped={true} />
